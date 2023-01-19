5c40b7091b6b18439fddfd9e4d1d11af870a39e1
Abyravka90 <asepcahya.nugraha75@gmail.com>
2023-01-19 09:19:15 +0700

    initial-commit

---
+++ assets/menu.php
@@ -0,0 +1,8 @@
+<div class="col-md-3">
+    <ul class="list-group">
+        <li class="list-group-item active">MAIN MENU</li>
+        <a href="logout" class="list-group-item" style="color: #212529;">Dashboard</a>
+        <a href="<?php $_SERVER['SERVER_NAME'] ?>/PHP-MYSQLI-AJAX/modul-siswa/dashboard.php" class="list-group-item" style="color: #212529;">Siswa</a>
+        <a href="<?php $_SERVER['SERVER_NAME'] ?>/PHP-MYSQLI-AJAX/modul-user/logout.php" class="list-group-item" style="color: #212529;">Logout</a>
+    </ul>
+</div>
